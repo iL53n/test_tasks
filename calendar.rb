4720f54f4cd13213c1b521330fe2a556932978e7
iL53n <ilsegribko@gmail.com>
2022-03-19 13:15:55 +0300

    refactoring

--- calendar.rb
+++ calendar.rb
@@ -13,37 +13,34 @@ hash_days = {}
 first_day.upto(last_day) { |day| hash_days[day.day] = day.strftime('%a') }
 
 # group by wday
-grouped_arr = hash_days.group_by { |day, wday| wday }
+grouped_hash = hash_days.group_by { |day| day.pop }.transform_values { |days| days.flatten }
 
 # sort by wday from DAYS_ORDER
-sorted_arr = grouped_arr.sort_by { |k, v| DAYS_ORDER.index(k) }
+sorted_arr = grouped_hash.sort_by { |k, v| DAYS_ORDER.index(k) }
 
 #  rendering current month's calendar with mark current day
 ## title
-
-print "\x1b[4m#{today.strftime("%B, %Y")}\x1b[0m\n"
+print "\x1b[4m#{today.strftime("%B %Y")}\x1b[0m\n"
 
 ## calendar
 indent = true
 
 sorted_arr.each do |wday, days|
   print wday
-  if days[0][0] != 1 && indent == true
+
+  if days[0] != 1 && indent == true
     print "   "
   else
     indent = false
   end
 
-  days.each do |v|
-    if v[0] > 9
-      day = " #{v[0]}"
-    else
-      day = "  #{v[0]}"
+  days.each do |value|
+    spaces = " " * (value > 9 ? 1 : 2)
+    str_day = spaces + value.to_s
+    current_day = "\x1b[1;31m#{str_day}\x1b[0m"
+
+    print value == today.day ? current_day : str_day
   end
 
-  current_day = "\x1b[1;31m#{day}\x1b[0m"
-
-  print v[0] == today.day ? current_day : day
-end
   puts
 end
